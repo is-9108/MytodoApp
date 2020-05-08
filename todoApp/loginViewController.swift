82fbe737faef9231af32eb04fb42278cf406800d
is-9108 <syouta103010@gmail.com>
2020-05-08 21:13:38 +0900

    UI調整

--- todoApp/loginViewController.swift
+++ todoApp/loginViewController.swift
@@ -7,24 +7,27 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+
 
 class loginViewController: UIViewController {
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if Auth.auth().currentUser == nil{
+            let signInViewController = self.storyboard?.instantiateViewController(withIdentifier: "signInViewController")
+            present(signInViewController!,animated: true,completion: nil)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
